d0add93e1ab7f43edcf69b4fb9406b2130b228af
hp <shafqatkb109@gmail.com>
2025-01-01 14:17:53 +0500

    e

--- presentation/src/main/java/com/apt/status/smart/compass/digital/presentation/di/presentationModule.kt
+++ presentation/src/main/java/com/apt/status/smart/compass/digital/presentation/di/presentationModule.kt
@@ -1,7 +1,9 @@
 package com.apt.status.smart.compass.digital.presentation.di
 
 import org.koin.androidx.viewmodel.dsl.viewModelOf
+import com.apt.status.smart.compass.digital.presentation.language.LanguageViewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
+    viewModelOf(::LanguageViewModel)
 }
